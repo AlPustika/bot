d7da118ecc5aa3dcba0eb20a0b5145ddbe4c98b7
alpustika <alpustika@gmail.com>
2018-01-21 21:33:06 +0300

    files

--- bot.py
+++ bot.py
@@ -7,8 +7,12 @@ upd = Updater(token)
 def start(bot, update):
     bot.sendMessage(update.message.chat_id, text='welcome on board')
 
+def help(bot, update):
+    update.message.reply_text('please read the help book')
+
 
 upd.dispatcher.add_handler(CommandHandler('start', start))
+upd.dispatcher.add_handler(CommandHandler('help', help))
 
 upd.start_polling()
 
