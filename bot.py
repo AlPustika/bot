571bdd1d942a2eaf8946f3214d7a09c09d4a293e
alpustika <alpustika@gmail.com>
2018-01-22 23:08:34 +0300

    files

--- bot.py
+++ bot.py
@@ -1,4 +1,6 @@
-from telegram.ext import Updater, CommandHandler, Filters, MessageHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, Filters, MessageHandler, CallbackQueryHandler
+
 import os
 
 upd = Updater(os.environ['TOKEN'])
@@ -8,18 +10,21 @@ def start(bot, update):
     bot.sendMessage(update.message.chat_id, text='welcome on board')
 
 def help(bot, update):
-    update.message.reply_text('please read the help book')
+    update.message.reply_text('you have been warned')
 
 def echo(bot, update):
     txt = update.message.text
     update.message.reply_text(txt.capitalize())
 
+def main():
+    upd.dispatcher.add_handler(CommandHandler('start', start))
+    upd.dispatcher.add_handler(CommandHandler('help', help))
+    upd.dispatcher.add_handler(MessageHandler(Filters.text, echo))
 
-upd.dispatcher.add_handler(CommandHandler('start', start))
-upd.dispatcher.add_handler(CommandHandler('help', help))
-upd.dispatcher.add_handler(MessageHandler(Filters.text, echo))
+    upd.start_polling()
 
-upd.start_polling()
+    #upd.idle()
 
-#upd.idle()
 
+if __name__== '__main__':
+    main()
