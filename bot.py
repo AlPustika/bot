fefad41381bd84a151c43c59a47984b909c4cf63
alpustika <alpustika@gmail.com>
2018-01-23 00:25:30 +0300

    files

--- bot.py
+++ bot.py
@@ -1,26 +1,54 @@
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, Filters, MessageHandler, CallbackQueryHandler
-
+import logging
 import os
 
 upd = Updater(os.environ['TOKEN'])
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def error(bot, update, error):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, error)
 
 
 def start(bot, update):
-    bot.sendMessage(update.message.chat_id, text='welcome on board')
+    cntr=['Country1','Country2','Country3''Country4']
+    #btn = [[InlineKeyboardButton('Country1', callback_data='Country1'),
+    #        InlineKeyboardButton('Country2', callback_data='Country2'),
+    #        InlineKeyboardButton('Country3', callback_data='Country3'),
+    #        InlineKeyboardButton('Country4', callback_data='Country4')]]
+    btn = [[InlineKeyboardButton('Country' + str(i), callback_data='Country' + str(i)) for i in range(1,4)]]
+    print (btn)#
+    reply_markup = InlineKeyboardMarkup(btn)
+    update.message.reply_text('Command: /buttons', reply_markup= reply_markup)
+
 
 def help(bot, update):
-    update.message.reply_text('you have been warned')
+    update.message.reply_text('you have been iop')
+
 
 def echo(bot, update):
     txt = update.message.text
     update.message.reply_text(txt.capitalize())
 
+
+def button(bot, update):
+    query = update.callback_query
+
+    bot.edit_message_text(text="Selected option: {}".format(query.data),
+                          chat_id=query.message.chat_id,
+                          message_id=query.message.message_id)
+
+
 def main():
     upd.dispatcher.add_handler(CommandHandler('start', start))
     upd.dispatcher.add_handler(CommandHandler('help', help))
     upd.dispatcher.add_handler(MessageHandler(Filters.text, echo))
-
+    upd.dispatcher.add_handler(CallbackQueryHandler(button))
+    upd.dispatcher.add_error_handler(error)
     upd.start_polling()
 
     #upd.idle()
@@ -28,3 +56,6 @@ def main():
 
 if __name__== '__main__':
     main()
+
+#l = range(1, 10)
+#print ([l[x:x+10] for #x in range(0, len(l), 1)])
