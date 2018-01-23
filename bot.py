67aaf2bf20a766045fed2208d7ffdd65abe12c43
alpustika <alpustika@gmail.com>
2018-01-23 21:07:27 +0300

    update buttons

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, Filters, MessageHandler, CallbackQueryHandler
 import logging
 import os
+import pandas as pd
 
 upd = Updater(os.environ['TOKEN'])
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -20,7 +21,16 @@ def start(bot, update):
     #        InlineKeyboardButton('Country2', callback_data='Country2'),
     #        InlineKeyboardButton('Country3', callback_data='Country3'),
     #        InlineKeyboardButton('Country4', callback_data='Country4')]]
-    btn = [[InlineKeyboardButton('Country' + str(i), callback_data='Country' + str(i)) for i in range(1,4)]]
+    #btn = [[InlineKeyboardButton('Country' + str(i), callback_data='Country' + str(i)) for i in range(1,5)]]
+    #cn = ['England', 'France', 'Spain', 'Belgium', 'Poland', 'Germany', 'Finland', 'Pakistan', 'India', 'Netherlands']
+    df = pd.read_csv('studios.csv')
+
+    cn = pd.unique(df.Country)
+
+    btn = [[InlineKeyboardButton(cn[i], callback_data=cn[i]),
+           InlineKeyboardButton(cn[i + 1], callback_data=cn[i + 1]),
+           InlineKeyboardButton(cn[i + 2], callback_data=cn[i + 2])] for i in range(0, len(cn)-2, 3)]
+
     print (btn)#
     reply_markup = InlineKeyboardMarkup(btn)
     update.message.reply_text('Command: /buttons', reply_markup= reply_markup)
