41ad02fac4dd8e95ff4c0b055d2b71624094c4b6
alpustika <alpustika@gmail.com>
2018-01-29 00:51:21 +0300

    tt

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, ReplyKeyboardRemove
 from telegram.ext import Updater, CommandHandler, Filters, MessageHandler, CallbackQueryHandler
 import logging
 import os
@@ -17,10 +17,7 @@ def error(bot, update, error):
 
 def start(bot, update):
 
-    btn = country()
-    reply_markup = InlineKeyboardMarkup(btn)
-    update.message.reply_text('Select country please', reply_markup=reply_markup)
-
+    update.message.reply_text('Select country or job position', reply_markup=ReplyKeyboardMarkup([['Country', 'Job Position']], resize_keyboard=True,one_time_keyboard=True))
 
 
 def country():
@@ -28,8 +25,8 @@ def country():
     df = pd.read_csv('studios.csv')  # read data
     all_cntry = pd.unique(df.Country)  # kill country with the same name
     all_cntry = sorted(all_cntry)  # sorting by alphabet
-    print(all_cntry)
-    rest = len(all_cntry) % size  # how many countes not in list (oatatok)
+    #print(all_cntry)
+    rest = len(all_cntry) % size  # how many countes not in list (ostatok)
     cnt = len(all_cntry) - rest  # length for cyckling
     ls = [[InlineKeyboardButton(all_cntry[i], callback_data=all_cntry[i]),
            InlineKeyboardButton(all_cntry[i + 1], callback_data=all_cntry[i + 1]),
@@ -41,24 +38,66 @@ def country():
                    InlineKeyboardButton(all_cntry[-2], callback_data=all_cntry[-2])])
     return ls
 
+
+def city(cntry):
+    size = 3
+    df = pd.read_csv('studios.csv')  # read data
+    all_city = pd.unique(df[df.Country == cntry][['City']].squeeze())
+    all_city = sorted(all_city)  # sorting by alphabet
+    #print(all_city)
+    rest = len(all_city) % size  # how many countes not in list (ostatok)
+    cnt = len(all_city) - rest  # length for cyckling
+    ls = [[InlineKeyboardButton(all_city[i], callback_data=all_city[i]),
+           InlineKeyboardButton(all_city[i + 1], callback_data=all_city[i + 1]),
+           InlineKeyboardButton(all_city[i + 2], callback_data=all_city[i + 2])] for i in range(0, cnt, size)]
+    if rest == 1:
+        ls.append([InlineKeyboardButton(all_city[-1], callback_data=all_city[-1])])#
+    elif rest == 2:
+        ls.append([InlineKeyboardButton(all_city[-1], callback_data=all_city[-1]),
+                   InlineKeyboardButton(all_city[-2], callback_data=all_city[-2])])
+
+    return ls
+
+
 def echo(bot, update):
     txt = update.message.text
-    update.message.reply_text(txt.capitalize())
+    update.message.reply_text('good buy', reply_markup=ReplyKeyboardRemove())
+    #print(update.callback_query)
+    if txt == 'Country':
+        btn = country()
+        reply_markup = InlineKeyboardMarkup(btn)
+        update.message.reply_text('Select country please', reply_markup=reply_markup)
+        #city = pd.unique(df[df.Country == cnt][['City']].squeeze())
 
 
 def button(bot, update):
     query = update.callback_query
-
     bot.edit_message_text(text="Selected option: {}".format(query.data),
                           chat_id=query.message.chat_id,
                           message_id=query.message.message_id)
+    print(query.data)
+    cities = city(query.data)
+    if cities:
+       # print(cities)
+        reply_markup2 = InlineKeyboardMarkup(cities)
+       #update.message.reply_text('Cities for selected Country:', reply_markup=reply_markup2)
+    keyboard = [[InlineKeyboardButton("Option 1", callback_data='1'),
+                 InlineKeyboardButton("Option 2", callback_data='2')],
 
+                [InlineKeyboardButton("Option 3", callback_data='3')]]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+#
+    #bot.edit_message_text('Please choose:')
+    #bot.sendMessage(chat_id=update.message.chat_id, text='hallo')
+    print('bot', bot)
+    print('update', update)
 
 def main():
 
     tok = os.environ['TOKEN']
     #print(tok)
-    upd = Updater('510826842:AAGLRHUfrT4CgteJ3hzMV7HjykfrMl1EZv0')
+    upd = Updater('492259029:AAEY4oqW4rGVNRLJdZx-Yt1UOyNN6e2SB-U')
     #print(upd)
     upd.dispatcher.add_handler(CommandHandler('start', start))
     upd.dispatcher.add_handler(CommandHandler('help', help))
--- tst.py
+++ tst.py
@@ -1,6 +1,9 @@
 import pandas as pd
 
 df = pd.read_csv('studios.csv')
-
+print(type(df.Country))
 all_cntry = pd.unique(df.Country)
-print(all_cntry)
+print(type(all_cntry))
+cnt = 'Poland'
+city = pd.unique(df[df.Country == cnt][['City']].squeeze())
+print(type(city),city)
